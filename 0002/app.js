a50797eeafda421536fb147e71a4ecf6a4fbe9d4
jason <bjason@gmail.com>
2017-06-13 01:32:56 -0700

    some ideas

--- 0002/app.js
+++ 0002/app.js
@@ -1,13 +1,46 @@
 var express = require("express"); 
 var app = express();
 
+// DB Stuff
+var mongoose = require("mongoose");
+var mongoDB = "mongodb://127.0.0.1/day2_ideas";
+var Schema = mongoose.Schema;
+
+var IdeaSchema = Schema( {
+  title: {type: String, required: true}
+});
+
+var Idea = mongoose.model('Idea', IdeaSchema);
+mongoose.connect(mongoDB);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-
+// Simple routes:
 app.get("/", function(req, res){
   res.send("Hello world");
 });
 
+app.get("/create-ideas", function(req, res){
+  var lilIdea = new Idea();
+  lilIdea.title = "Some idea";
+  lilIdea.save(function(){
+    res.send("Created...");
+  });
+});
+
+app.get("/all-ideas", function(req, res){
+  var ideas = Idea.find({}, function(error, ideas){
+   res.json({
+    results: ideas
+   });
+  });
+});
+
+// External routes
+var ideaRoutes = require("./ideas");
+app.use("/ideas", ideaRoutes);
 
 app.listen(3000, function(){
   console.log("Started server on port 3000");
