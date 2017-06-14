b889a46a21551a6258e9201bba9fede0b6162a56
jason <bjason@gmail.com>
2017-06-14 02:33:43 -0700

    rendering with simple react

--- 0003/main.js
+++ 0003/main.js
@@ -1,7 +1,32 @@
 console.log("hello");
+var items = [
+  {
+    title: 'One',
+    complete: false
+  },
+  {
+    title: 'Two',
+    complete: false
+  },
+  {
+    title: 'Three',
+    complete: false
+  }
+];
+
+var render =  function(){
+
+  var itemEls = [];
+  items.forEach(function(item){
+    itemEls.push(React.createElement(
+      'h2', null, item.title
+    ));
+  });
+  ReactDOM.render(
+    React.createElement('div', null, itemEls),
+    document.getElementById('root')
+  );
+}
 
 
-ReactDOM.render(
-  React.createElement('h1', null, 'Hello React'),
-  document.getElementById('root')
-);
+render();
