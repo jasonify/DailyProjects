8d410038e7516319bcbe640d8e9bf3b8a3ba1cff
jason <bjason@gmail.com>
2017-06-14 02:47:34 -0700

    basics

--- 0003/main.js
+++ 0003/main.js
@@ -2,26 +2,40 @@ console.log("hello");
 var items = [
   {
     title: 'One',
+    color: 'blue',
     complete: false
   },
   {
     title: 'Two',
+    color: 'red',
     complete: false
   },
   {
     title: 'Three',
+    color: 'green',
     complete: false
   }
 ];
 
+// Auto select first element
+var  selectedEl = items[0].title;
+
+var render;
 var ItemEl = React.createClass({
+  handleClick: function(){
+    console.log("this", this);
+    selectedEl = this.props.title;
+    render();
+
+  },
   render: function(){
-    return React.createElement('p', {className: 'item'}, this.props.title);
+    return React.createElement('p', {className: 'item', onClick: this.handleClick}, this.props.title);
   }
 });
 
-var render =  function(){
+render =  function(){
   var itemEls = [];
+  itemEls.push(React.createElement('h1', null, selectedEl));
   items.forEach(function(item){
     itemEls.push(React.createElement(
       ItemEl, item, 
